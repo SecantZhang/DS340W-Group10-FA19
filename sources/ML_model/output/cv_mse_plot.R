a719391e4248ae056c33b8dd03e016a489278830
Wilson (Zheng) Zhang <abovesummitwz@gmail.com>
2019-11-19 15:38:11 -0500

    Updated the cross-validation script.

--- sources/ML_model/output/cv_mse_plot.R
+++ sources/ML_model/output/cv_mse_plot.R
@@ -7,3 +7,20 @@ mse %>% ggplot(aes(x = validation_fold, y = MSE)) +
     labs(x = "Iteration / Validation Fold Number in k-Fold Cross Validation", 
          y = "Mean Squared Error Value") + 
     ggtitle("Mean Square Error of Different Tracks in Each CV Iteration")
+
+mse_hist <- read.csv("output/mse_hist_marks_model.csv", header=TRUE)
+mse_hist %>% ggplot(aes(x = Iteration, shape = ideas)) + 
+    geom_point(aes(y = predicted, col = "Predicted")) + 
+    geom_point(aes(y = avocado, col = "Avocado")) + 
+    geom_point(aes(y = curr_impute, col = "Curr_impute")) + 
+    facet_wrap(~Mark_id) + 
+    labs(x = "Iteration / Validation Fold Number in k-Fold Cross Validation", 
+         y = "Mean Squared Error Value")
+
+mse_hist %>% select(-curr_impute) %>% 
+    ggplot(aes(x = Iteration, shape = ideas)) + 
+    geom_point(aes(y = predicted, col = "Predicted")) + 
+    geom_point(aes(y = avocado, col = "Avocado")) + 
+    facet_wrap(~Mark_id) + 
+    labs(x = "Iteration / Validation Fold Number in k-Fold Cross Validation", 
+         y = "Mean Squared Error Value")
